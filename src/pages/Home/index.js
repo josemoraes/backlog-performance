72d026f1c3d1193965de615aee19b238d12e7a73
Jose Carlos De Moraes Filho <jose.filho@sistemafiep.org.br>
2021-07-26 20:18:28 -0300

    feat: Defining visual

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,9 @@
 const template = require("./index.html");
-import logo from '../../assets/images/activity.svg';
 
 const _getTemplate = () => {
   return template;
 };
 
-const _page_events = () => {
-
-};
+const _page_events = () => {};
 
 export default { _getTemplate, _page_events };
